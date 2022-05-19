dfae81b364ac72fbe203d5607cb5db6419673421
Juan Pablo Bessone <jbessone@facultad.sanfrancisco.utn.edu.ar>
2022-05-19 18:56:24 -0300

    add fluentvalidator

---
+++ TP1_Diseño/PartnerValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace TP1_Diseño
+{
+    public class PartnerVañidator : AbstractValidator<Partner>
+    {
+
+    }
+}
